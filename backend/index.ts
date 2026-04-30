094004cce3b01584847eb6976512d094bd19cf69
Blaze5333 <mustafachaiwala2003@gmail.com>
2026-04-30 12:34:39 +0530

    feat: implement socket initialization and authentication for real-time messaging

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,11 @@
 import app from "./src/app";
 import { connectDB } from "./src/config/database";
+import {createServer} from "http";
+
 const PORT=process.env.PORT || 3000;
+const httpServer=createServer(app);
 connectDB().then(() => {
-  app.listen(PORT, () => {
+  httpServer.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 });
